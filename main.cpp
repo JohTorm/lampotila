5c39edf1ce1c6d2ce0a12de3af1f605364445497
Johannes Törmänen <johannes.tormanen@hotmail.fi>
2019-11-26 11:59:45 +0200

    HC-SR04 ultraäänisensori lisätty

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include "mbed.h"
-
+#include "ultrasonic.h"
 
 Timer timer;
-Serial pc(PC_4, PC_5, 9600); //datan lahettamista nucleolle varten vaihdettu USBRX,USBTX
-
+//Serial pc(PC_4, PC_5, 9600); //datan lahettamista nucleolle varten vaihdettu USBRX,USBTX
+Serial pc(USBTX, USBRX, 9600);
 AnalogIn Analog(A0);
 
 
@@ -12,12 +12,31 @@ float resisX = 0.0;   //kOhm
 float resisKiintea = 12.0; //kOhm
 float kayttoJannite = 3.3;
 float lampotila = 0.0;
+void dist();
+
+ void dist(int distance)
+{
+    
+	if(distance >= 200)
+	{
+		pc.printf("xD\r\n");
+	}
+	//put code here to happen when the distance is changed
+    pc.printf("Distance changed to %dmm\r\n", distance);
+	
+}
+ 
+ 
+ ultrasonic mu(D8, D9, .1, 1, &dist);    //Set the trigger pin to D8 and the echo pin to D9
+                                        //have updates every .1 seconds and a timeout after 1
+                                        //second, and call dist when the distance changes
 
 int main()
 {
 
 timer.start();
 timer.reset();
+mu.startUpdates();//start mesuring the distance
         while(true)
         {
 			      
@@ -31,6 +50,8 @@ timer.reset();
 				pc.printf("%3.3f",lampotila);
 				pc.printf("\r\n");
 				
-				//wait(0.5);
+				mu.checkDistance();     //call checkDistance() as much as possible, as this is where
+                                //the class checks if dist needs to be called.
+				wait(0.1);
         }
 }
